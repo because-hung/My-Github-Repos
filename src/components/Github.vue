3f8d294ba3a053b32c0f79d27ffa8d6fe23fbe38
洪瑛蔚 <4a33a006@stust.edu.tw>
2021-01-25 18:58:50 +0800

    run build

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -32,14 +32,13 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       Data: [],
       newData: [],
       scrollData: [],
-      Num: 0,  
+      Num: 0,
       Counts: 6,
     };
   },
@@ -51,39 +50,43 @@ export default {
         vm.Data = response.data;
         vm.length = response.data.length;
 
-        if (vm.Num == 0) {  //判斷是不是剛載入頁面
+        if (vm.Num == 0) {
+          //判斷是不是剛載入頁面
           vm.newData = vm.Data.slice(vm.Num, vm.Counts); //初始值 Num = 0 Counts = 6  意思是最開始的畫面只呈現6個資料
         } else {
           vm.scrollData = vm.Data.slice(vm.Num, vm.Counts);
 
           vm.newData.push(...vm.scrollData); //當畫面滑到最底下 增加6筆資料
-        //   console.log(vm.newData);
+          //   console.log(vm.newData);
           vm.scrollData = []; //清空
         }
 
-        vm.Num += 6;    //之後當畫面滑到最底下 每次增加6筆資料
+        vm.Num += 6; //之後當畫面滑到最底下 每次增加6筆資料
         vm.Counts += 6;
       });
     },
     Scroll() {
-      const clientHeight = document.documentElement.clientHeight;  //網頁內容的高度 
-      const pageHeight = document.documentElement.scrollHeight;   //整個頁面的高度
-      const scrollY = window.scrollY;  //垂直捲軸滾動的高度
-      if (scrollY + clientHeight >= pageHeight) {  //垂直捲軸滾動的高度 + 網頁內容的高度 > 整個頁面的高度 就增加資料
-        this.getData();  
+      const clientHeight = document.documentElement.clientHeight; //網頁內容的高度
+      const pageHeight = document.documentElement.scrollHeight; //整個頁面的高度
+      const scrollY = window.scrollY; //垂直捲軸滾動的高度
+      if (scrollY + clientHeight >= pageHeight) {
+        //垂直捲軸滾動的高度 + 網頁內容的高度 > 整個頁面的高度 就增加資料
+        this.getData();
       }
     },
   },
   created() {
     this.getData();
-    window.addEventListener("scroll", () => {  //監聽畫面有沒有滑到最下面
+    window.addEventListener("scroll", () => {
+      //監聽畫面有沒有滑到最下面
       this.Scroll();
     });
   },
-  beforeDestroy(){
-    window.removeEventListener("scroll",() => { // 如果換頁的話 移除監聽
-        this.Scroll();
+  beforeDestroy() {
+    window.removeEventListener("scroll", () => {
+      // 如果換頁的話 移除監聽
+      this.Scroll();
     });
-     }
+  },
 };
 </script>
